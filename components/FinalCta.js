eeb156b7d8c0d5232ee05ede6ac00e7f6c2c4e39
OscarDME <oscarmadriz25@gmail.com>
2026-04-02 23:00:10 -0600

    chore: actualizar imagen mockup a formato webp

--- components/FinalCta.js
+++ components/FinalCta.js
@@ -72,7 +72,7 @@ export default function FinalCta() {
         {/* Product mockup */}
         <div className="flex justify-center mb-10">
           <Image
-            src="/mockup/Polonia.png"
+            src="/mockup/polonia.webp"
             alt={finalCta.mockupAlt}
             width={320}
             height={320}
